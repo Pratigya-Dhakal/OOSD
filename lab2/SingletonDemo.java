2fd3731ce8d637e0fdc31753d3e3148fe1651ebe
Pratigya-Dhakal <pratigyadhakal4444@gmail.com>
2022-07-14 08:56:15 +0545

    Lab 2 Files added

---
+++ lab2/SingletonDemo.java
@@ -0,0 +1,28 @@
+
+class Singleton {
+    // Early initializing a singleton
+    private static Singleton instance = new Singleton();
+
+    // making constructor private to create a singleton
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        return instance;
+    }
+}
+
+public class SingletonDemo {
+    public static void main(String[] args) {
+        Singleton test1 = Singleton.getInstance();
+        Singleton test2 = Singleton.getInstance();
+
+        System.out.println(test1.hashCode());
+        System.out.println(test2.hashCode());
+
+        /**
+         * output:
+         * 746292446
+         * 746292446
+         */
+    }
+}
