f8109c4458f52e3e94322186b727ce704eee579d
Pratigya-Dhakal <pratigyadhakal4444@gmail.com>
2022-07-14 08:56:46 +0545

    Lab 3 files added

---
+++ lab3/PrototypeDemo.java
@@ -0,0 +1,82 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+interface EmployeeRecord {
+    public EmployeeRecord getClone();
+}
+
+class CollegeTeacher implements EmployeeRecord {
+    private int id = 0;
+    private String name;
+    private String address;
+
+    public CollegeTeacher(int id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public void showRecord () {
+        System.out.println("");
+        System.out.println("Teacher Employee Record");
+        System.out.println("------------------------------");
+        System.out.println("Id\tname\taddress\tsalary");
+        System.out.println(id+"\t"+name+"\t"+address+"\tRs. 40000");
+        System.out.println("");
+    }
+    
+    @Override
+    public EmployeeRecord getClone() {
+        this.id++;
+        return new CollegeTeacher(this.id, this.name, this.address);
+    }
+}
+
+public class PrototypeDemo {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter an ID: ");
+        int id = Integer.parseInt(br.readLine());
+        System.out.println("\n");
+
+        System.out.print("Enter teacher name: ");
+        String name = br.readLine();
+        System.out.println("\n");
+
+        System.out.print("Enter address: ");
+        String address = br.readLine();
+        System.out.println("\n");
+
+        CollegeTeacher ct1 = new CollegeTeacher(id, name, address);
+        ct1.showRecord();
+
+        CollegeTeacher ct2 = (CollegeTeacher) ct1.getClone();
+        ct2.showRecord();
+    }
+}
+/*
+output
+Enter an ID: 25
+
+
+Enter teacher name: Zena Poudel
+
+
+Enter address: Parsyang
+
+
+
+Teacher Employee Record
+------------------------------
+Id      name    address salary
+25      Zena Poudel     Parsyang        Rs. 40000
+
+
+Teacher Employee Record
+------------------------------
+Id      name    address salary
+26      Zena Poudel     Parsyang        Rs. 40000
+*/
